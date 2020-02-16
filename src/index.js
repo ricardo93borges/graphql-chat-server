3e955aaecb2cbf51ee504085ada95d363c11dce5
ricardo93borges <ricardo93borges@gmail.com>
2020-02-16 19:44:26 -0300

    refactor to get connection params from web sockets connections

--- src/index.js
+++ src/index.js
@@ -5,19 +5,27 @@ import models from './model/index'
 
 const pubsub = new PubSub()
 
+const getUser = async (req, connection) => {
+  let user = null
+
+  if (req && req.headers.authorization) {
+    const token = req.headers.authorization.replace('Bearer ', '')
+    user = await models.user.getUserByToken(token)
+  } else if (connection && connection.context.Authorization) {
+    const token = connection.context.Authorization.replace('Bearer ', '')
+    user = await models.user.getUserByToken(token)
+  }
+
+  return user
+}
+
 const server = new ApolloServer({
   schema,
   context: async ({ req, res, connection }) => {
-    let user = null
-    if (req.headers.authorization) {
-      const token = req.headers.authorization.replace('Bearer ', '')
-      user = await models.user.getUserByToken(token)
-    }
-
     return {
       models,
       pubsub,
-      user
+      user: await getUser(req, connection)
     }
   }
 })
